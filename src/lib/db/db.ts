c04b97440b4244bda2f41e5f09adf0c41c0232c7
Martyna Kweczke <martynakweczke@gmail.com>
2026-09-25 21:41:53 +0200

    improve file

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -3,6 +3,8 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "@/lib/db/schema";
 
+type Db = ReturnType<typeof createDb>;
+
 function connectionString(): string {
   const url = process.env.DATABASE_URL;
 
@@ -15,7 +17,18 @@ function connectionString(): string {
   return url;
 }
 
-export const db = drizzle(neon(connectionString()), {
-  schema,
-  casing: "snake_case",
+function createDb() {
+  return drizzle(neon(connectionString()), {
+    schema,
+    casing: "snake_case",
+  });
+}
+
+let instance: Db | undefined;
+
+export const db = new Proxy({} as Db, {
+  get(_target, property) {
+    instance ??= createDb();
+    return Reflect.get(instance, property, instance) as unknown;
+  },
 });
